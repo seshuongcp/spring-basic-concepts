e63f2ec40a67db7de5e0767624c219bd41b46a91
SeshuOne <seshu.ep@gmail.com>
2025-11-29 12:39:25 +0530

    feat: add constructor-based DI using GreetServie and GreetingRepository

---
+++ src/main/java/com/example/GreetingService.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class GreetingService {
+
+    private final GreetingRepository greetingRepository;
+
+    public GreetingService(GreetingRepository greetingRepository){
+        System.out.println("GreetingService: Constructor called");
+        this.greetingRepository= greetingRepository;
+    }
+
+
+    String greet(){
+        return greetingRepository.getGreetingMessage();
+    }
+}
